cb552ce24398be20bb6de0b0ac85c92f586e8290
Mark Montoya <bloggermark@sbcglobal.net>
2021-09-28 21:24:21 -0700

    added data to template

---
+++ data.js
@@ -0,0 +1,19 @@
+const cars = [
+  {
+    make: "Toyota",
+    model: "Tercel",
+    year: "1986",
+  },
+  {
+    make: "Saturn",
+    model: "Relay",
+    year: "1996",
+  },
+  {
+    make: "MINI",
+    model: "Cooper",
+    year: "2016",
+  },
+]
+
+module.exports = { cars }
--- myapp.js
+++ myapp.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
 const handlebars = require("express-handlebars")
+const { cars } = require("./data.js")
 app.use(express.static("public"))
 app.set("view engine", "handlebars")
 app.engine(
@@ -10,8 +11,7 @@ app.engine(
     layoutsDir: `${__dirname}/views/layouts`,
   })
 )
-app.get("/", (req, res) => {
-  // res.send("Hello world")
-  res.render("main", { layout: "index" })
+app.get("/", (req, res, next) => {
+  res.render("main", { layout: "index", cars })
 })
 app.listen(port, () => console.log(`App is listening on port ${port}`))
